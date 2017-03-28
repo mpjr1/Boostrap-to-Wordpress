6f8d62179711911c965c4e9208e746b1cd04bef5
mp_jr1 <mapg.jr@gmail.com>
2017-03-28 20:26:20 +0000

    Assets files in dynamic WordPress section completed

---
+++ wp-content/themes/bootstrap2wordpress/assets/js/main.js
@@ -0,0 +1,27 @@
+// JavaScript File
+
+$(function(){
+    //Cache the window object
+    var $window = $(window);
+    
+    
+    //Parallax background effect
+    $('selection[data-type="background")]').each(function(){
+        var $bgobj = $(this); //assigning the object
+        $(window).scroll(function(){
+            // Scroll the background at var speed
+            //the y position is a negative value because we're
+            // scrolling it up
+            
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+            
+            //Put together our final background position
+            var coords = '50% ' + yPos + 'px';
+            
+            //Move Background
+            $bgobj.css({ backgroundPosition: coords});
+            
+        }); //end window scroll
+    });
+    
+});
--- wp-content/themes/bootstrap2wordpress/header.php
+++ wp-content/themes/bootstrap2wordpress/header.php
@@ -16,7 +16,22 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<!--Font Awesome-->
+<link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<!--Google Fonts-->
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+<!--Bootstrap Core CSS-->
+<link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+
 <?php wp_head(); ?>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
